1d67724df29f7aefda9e1eee75bc326b2a1332c7
Uliana Seredenko <uliana1331@gmail.com>
2025-02-20 16:16:16 +0100

    create read and write contacts fns

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,12 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    //getting contacts
+    const data = await fs.readFile(PATH_DB);
+    return JSON.parse(data);
+  } catch (error) {
+    console.log('Error in reading contacts: ', error.message);
+  }
+};
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,10 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts));
+  } catch (error) {
+    console.log('Error in creating contacts: ', error.message);
+  }
+};
