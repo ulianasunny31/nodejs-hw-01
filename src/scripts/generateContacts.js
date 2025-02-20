51f9314636e7398e837031b69244acc46d0fb228
Uliana Seredenko <uliana1331@gmail.com>
2025-02-20 16:16:43 +0100

    create generate contacts fn

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,24 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
 
-generateContacts(5);
+const generateContacts = async (number) => {
+  try {
+    //initializing empty contact list from the db
+    const contacts = await readContacts();
+
+    // iteration over the right number of contacts
+    for (let i = 0; i < number; i++) {
+      //creating and adding to the list
+      const contact = createFakeContact();
+      contacts.push(contact);
+    }
+
+    //adding to the contacts db
+    await writeContacts(contacts);
+  } catch (error) {
+    console.log('Error in generating contacts: ', error.message);
+  }
+};
+
+generateContacts(2);
