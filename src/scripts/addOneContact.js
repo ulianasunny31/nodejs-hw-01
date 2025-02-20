b2ff02359b4d6949d6a2d97301746617ed6c605e
Uliana Seredenko <uliana1331@gmail.com>
2025-02-20 16:17:15 +0100

    create add1 and getAll fn

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,23 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  try {
+    //initializing empty contact list from the db
+    const contacts = await readContacts();
+
+    //creating a new contact
+    const newContact = createFakeContact();
+
+    //adding the new contact to the contact list
+    contacts.push(newContact);
+
+    //adding to the contacts db
+    await writeContacts(contacts);
+  } catch (error) {
+    console.log('Error in creating one contact: ', error.message);
+  }
+};
 
 addOneContact();
--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,13 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const getAllContacts = async () => {
+  try {
+    //getting the list from the db
+    const res = await readContacts();
+    return res;
+  } catch (error) {
+    console.log('Error in getting all contacts: ', error.message);
+  }
+};
 
 console.log(await getAllContacts());
